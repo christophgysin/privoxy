00c80ad4aa76d35a8a591b5f9ef95c25596f050d
Andreas Oesterhelt <oes@users.sourceforge.net>
2001-06-29 13:30:37 +0000

    - Introduced enlist_unique_header() - Removed logentry from cancelled commit

--- list.h
+++ list.h
@@ -36,59 +36,13 @@
  *
  * Revisions   :
  *    $Log$
+ *    Revision 1.4  2001/06/29 13:30:37  oes
+ *    - Introduced enlist_unique_header()
+ *    - Removed logentry from cancelled commit
+ *
  *    Revision 1.3  2001/06/03 11:03:48  oes
- *    Makefile/in
- *
- *    introduced cgi.c
- *
- *    actions.c:
- *
- *    adapted to new enlist_unique arg format
- *
- *    conf loadcfg.c
- *
- *    introduced confdir option
- *
- *    filters.c filtrers.h
- *
- *     extracted-CGI relevant stuff
- *
- *    jbsockets.c
- *
- *     filled comment
- *
- *    jcc.c
- *
- *     support for new cgi mechansim
- *
- *    list.c list.h
- *
- *    functions for new list type: "map"
- *    extended enlist_unique
- *
- *    miscutil.c .h
- *    introduced bindup()
- *
- *    parsers.c parsers.h
- *
- *    deleted const struct interceptors
- *
- *    pcrs.c
- *    added FIXME
- *
- *    project.h
- *
- *    added struct map
- *    added struct http_response
- *    changes struct interceptors to struct cgi_dispatcher
- *    moved HTML stuff to cgi.h
- *
- *    re_filterfile:
- *
- *    changed
- *
- *    showargs.c
- *    NO TIME LEFT
+ *    introduced functions for new list type "map": map(), lookup(),
+ *    free_map(), and extended enlist_unique
  *
  *    Revision 1.2  2001/06/01 18:49:17  jongfoster
  *    Replaced "list_share" with "list" - the tiny memory gain was not
@@ -122,16 +76,18 @@ extern "C" {
 
 extern void enlist(struct list *h, const char *s);
 extern void enlist_unique(struct list *header, const char *str, int n);
+extern void enlist_unique_header(struct list *header, const char *name, const char *value);
 extern void enlist_first(struct list *header, const char *str);
+
 extern int   list_remove_item(struct list *header, const char *str);
 
 extern void  list_append_list_unique(struct list *dest, const struct list *src);
 extern void  list_append_list_unique(struct list *dest, const struct list *src);
 extern int   list_remove_list(struct list *header, const struct list *to_remove);
+
 extern void  list_duplicate(struct list *dest, const struct list *src);
 
 extern void  destroy_list(struct list *h);
-
 extern char *list_to_text(struct list *h);
 
 extern struct map* map(struct map* map, char *name, int nc, char *value, int vc);
