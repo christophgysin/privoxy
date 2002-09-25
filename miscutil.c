9fff34a79b787ccbc57613b0b7d10d4491c5c0d4
Andreas Oesterhelt <oes@users.sourceforge.net>
2002-09-25 12:58:51 +0000

    Made strcmpic and strncmpic safe against NULL arguments (which are now treated as empty strings).

--- miscutil.c
+++ miscutil.c
@@ -36,6 +36,10 @@ const char miscutil_rcs[] = "$Id$";
  *
  * Revisions   :
  *    $Log$
+ *    Revision 1.37.2.1  2002/09/25 12:58:51  oes
+ *    Made strcmpic and strncmpic safe against NULL arguments
+ *    (which are now treated as empty strings).
+ *
  *    Revision 1.37  2002/04/26 18:29:43  jongfoster
  *    Fixing this Visual C++ warning:
  *    miscutil.c(710) : warning C4090: '=' : different 'const' qualifiers
@@ -399,6 +403,9 @@ char *safe_strerror(int err)
  *********************************************************************/
 int strcmpic(const char *s1, const char *s2)
 {
+   if (!s1) s1 = "";
+   if (!s2) s2 = "";
+
    while (*s1 && *s2)
    {
       if ( ( *s1 != *s2 ) && ( ijb_tolower(*s1) != ijb_tolower(*s2) ) )
@@ -429,7 +436,9 @@ int strcmpic(const char *s1, const char *s2)
 int strncmpic(const char *s1, const char *s2, size_t n)
 {
    if (n <= 0) return(0);
-
+   if (!s1) s1 = "";
+   if (!s2) s2 = "";
+   
    while (*s1 && *s2)
    {
       if ( ( *s1 != *s2 ) && ( ijb_tolower(*s1) != ijb_tolower(*s2) ) )
