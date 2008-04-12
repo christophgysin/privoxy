61d59d226d238d81a32b2983e6dac23ce7f25e6d
Fabian Keil <fabiankeil@users.sourceforge.net>
2008-04-12 12:29:22 +0000

    In get_final_results(), quote with quote() and fix spelling in three comments.

--- tools/privoxy-regression-test.pl
+++ tools/privoxy-regression-test.pl
@@ -703,7 +703,7 @@ sub get_final_results ($) {
     my %final_results = ();
     my $final_results_reached = 0;
 
-    die "Unacceptable characterss in $url" if $url =~ m@[\\'"]@;
+    die "Unacceptable characters in $url" if $url =~ m@[\\'"]@;
     # XXX: should be URL-encoded properly
     $url =~ s@%@%25@g;
     $url =~ s@\s@%20@g;
@@ -711,7 +711,7 @@ sub get_final_results ($) {
     $url =~ s@:@%3A@g;
     $url =~ s@/@%2F@g;
 
-    $curl_parameters .= "'" . PRIVOXY_CGI_URL . 'show-url-info?url=' . $url . "'";
+    $curl_parameters .= quote(PRIVOXY_CGI_URL . 'show-url-info?url=' . $url);
 
     foreach (@{get_cgi_page_or_else($curl_parameters)}) {
 
@@ -726,10 +726,10 @@ sub get_final_results ($) {
             
             if (defined $parameter) {
                 # In case the caller needs to check
-                # the action and it's parameter
+                # the action and its parameter
                 $final_results{$action . $parameter} = 1;
             }
-            # In case the action doesn't have paramters
+            # In case the action doesn't have parameters
             # or the caller doesn't care for the parameter.
             $final_results{$action} = 1;
         }
