e9ff4096160f19ec96dd144e317ca5d28979b8a9
Fabian Keil <fabiankeil@users.sourceforge.net>
2009-07-05 12:02:25 +0000

    Mention the offending socket in the 'Empty server or forwarder response' message.

--- jcc.c
+++ jcc.c
@@ -2136,7 +2136,8 @@ static void chat(struct client_state *csp)
             /* Did we actually get anything? */
             if (NULL == csp->headers->first)
             {
-               log_error(LOG_LEVEL_ERROR, "Empty server or forwarder response.");
+               log_error(LOG_LEVEL_ERROR,
+                  "Empty server or forwarder response received on socket %d.", csp->sfd);
                log_error(LOG_LEVEL_CLF, "%s - - [%T] \"%s\" 502 0", csp->ip_addr_str, http->cmd);
                send_crunch_response(csp, error_response(csp, "no-server-data"));
                free_http_request(http);
