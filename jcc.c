c0afc0ac723eeb0ed63310cee1efa4bddca3cfc4
Fabian Keil <fabiankeil@users.sourceforge.net>
2009-07-07 16:42:26 +0000

    Revert r261 as it would sabotage NTLM authentication.

--- jcc.c
+++ jcc.c
@@ -1632,7 +1632,6 @@ static void chat(struct client_state *csp)
 
 #ifdef FEATURE_CONNECTION_KEEP_ALIVE
    if ((csp->sfd != JB_INVALID_SOCKET)
-      && !(csp->flags & CSP_FLAG_SERVER_SOCKET_TAINTED)
       && socket_is_still_usable(csp->sfd)
       && connection_destination_matches(&csp->server_connection, http, fwd))
    {
