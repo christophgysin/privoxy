ae410e09fb3593d16204c868fe71857ed2a7c41e
Fabian Keil <fabiankeil@users.sourceforge.net>
2009-07-14 18:40:48 +0000

    Accept and ignore: The server didn't specify how long the connection will stay open. Assume it's only a second.

--- tools/privoxy-log-parser.pl
+++ tools/privoxy-log-parser.pl
@@ -1600,7 +1600,8 @@ sub handle_loglevel_connect ($) {
              $c =~ m/^Complete client request received/ or
              $c =~ m/^Possible pipeline attempt detected./ or
              $c =~ m/^POST request detected. The connection will not be kept alive./ or
-             $c =~ m/^The server still wants to talk, but the client hung up on us./) {
+             $c =~ m/^The server still wants to talk, but the client hung up on us./ or
+             $c =~ m/^The server didn't specify how long the connection will stay open/) {
 
         # Looks like we reached the end of the last chunk. We better stop reading.
         # Looks like we read the end of the last chunk together with the server \
@@ -1613,6 +1614,7 @@ sub handle_loglevel_connect ($) {
         #  kept alive and we will only serve the first request.
         # POST request detected. The connection will not be kept alive.
         # The server still wants to talk, but the client hung up on us.
+        # The server didn't specify how long the connection will stay open. Assume it's only a second.
 
     } else {
 
