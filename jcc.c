30694e6670a8b11bf2b128241d16594e0938faa4
Fabian Keil <fabiankeil@users.sourceforge.net>
2009-08-19 16:02:53 +0000

    In chat(), set CSP_FLAG_CLIENT_REQUEST_COMPLETELY_READ after csp->expected_client_content_length reaches zero so we don't have to deal with the next request until we're ready again.

--- jcc.c
+++ jcc.c
@@ -1925,6 +1925,12 @@ static void chat(struct client_state *csp)
                "Expected client content length set to %llu "
                "after reading %d bytes.",
                csp->expected_client_content_length, len);
+            if (csp->expected_client_content_length == 0)
+            {
+               log_error(LOG_LEVEL_CONNECT,
+                  "Done reading from the client.");
+               csp->flags |= CSP_FLAG_CLIENT_REQUEST_COMPLETELY_READ;
+            }
          }
 #endif /* def FEATURE_CONNECTION_KEEP_ALIVE */
 
