c48e06b698d105abbf654f0b2de7e3c7624cfc25
Fabian Keil <fabiankeil@users.sourceforge.net>
2010-02-11 13:59:09 +0000

    Bump BROWSER_FOO variables for Firefox 3.6.

--- tools/uagen.pl
+++ tools/uagen.pl
@@ -56,9 +56,9 @@ use constant {
    # are too lazy to check, but want to change them anyway, take the values you
    # see in the "Help/About Mozilla Firefox" menu.
 
-   BROWSER_VERSION                   => "3.5.7",
-   BROWSER_REVISION                  => '1.9.1.7',
-   BROWSER_RELEASE_DATE              => '20100106',
+   BROWSER_VERSION                   => "3.6",
+   BROWSER_REVISION                  => '1.9.2',
+   BROWSER_RELEASE_DATE              => '20100121',
 };
 
 use constant LANGUAGES => qw(
