449ccfbbd000e201212107d079842ad5ca9577dc
Fabian Keil <fabiankeil@users.sourceforge.net>
2010-06-13 12:26:32 +0000

    In client_if_modified_since(), limit the scope of the variables hours, minutes and seconds.

--- parsers.c
+++ parsers.c
@@ -3153,7 +3153,6 @@ static jb_err client_if_modified_since(struct client_state *csp, char **header)
    struct tm *timeptr = NULL;
    time_t tm = 0;                  
    const char *newval;
-   long int hours, minutes, seconds;
    char * endptr;
    
    if ( 0 == strcmpic(*header, "If-Modified-Since: Wed, 08 Jun 1955 12:00:00 GMT"))
@@ -3188,6 +3187,7 @@ static jb_err client_if_modified_since(struct client_state *csp, char **header)
          }
          else
          {
+            long int hours, minutes, seconds;
             long int rtime = strtol(newval, &endptr, 0);
             const int negative_range = (rtime < 0);
 
