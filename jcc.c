5e8a8f2273ca9dbeac7c4117b1acaad6607be54d
Fabian Keil <fabiankeil@users.sourceforge.net>
2010-07-12 16:01:23 +0000

    In server_response_is_complete(), expect no body if the status code is 204.
    Should fix #3022042 and #3025553, reported by a user with no visible name.
    
    Most likely also fixes a bunch of other AJAX-related problem
    reports we closed in the past due to insufficient information.

--- jcc.c
+++ jcc.c
@@ -1004,7 +1004,7 @@ static int server_response_is_complete(struct client_state *csp,
       content_length_known = TRUE;
    }
 
-   if (csp->http->status == 304)
+   if (csp->http->status == 204 || csp->http->status == 304)
    {
       /*
        * Expect no body. XXX: incomplete "list" of status codes?
