d953eb1c4d07579a612c7f5e9577edb82e91fd4a
Fabian Keil <fabiankeil@users.sourceforge.net>
2010-11-08 17:53:29 +0000

    Ignore: Merged multiple header lines to: 'X-FORWARDED-PROTO: http X-HOST: 127.0.0.1'

--- tools/privoxy-log-parser.pl
+++ tools/privoxy-log-parser.pl
@@ -911,6 +911,7 @@ sub handle_loglevel_header ($) {
           or $c =~ m/^Removing 'Connection: close' to imply keep-alive./
           or $c =~ m/^keep-alive support is disabled/
           or $c =~ m/^Continue hack in da house/
+          or $c =~ m/^Merged multiple header lines to:/
             )
     {
         # XXX: Some of these may need highlighting
@@ -957,6 +958,7 @@ sub handle_loglevel_header ($) {
         # Removing 'Connection: close' to imply keep-alive.
         # keep-alive support is disabled. Crunching: Keep-Alive: 300.
         # Continue hack in da house.
+        # Merged multiple header lines to: 'X-FORWARDED-PROTO: http X-HOST: 127.0.0.1'
 
     } elsif ($c =~ m/^scanning headers for:/) {
 
