e15fef36b6ea977d58c092ea246ff58adcbd63af
Fabian Keil <fabiankeil@users.sourceforge.net>
2011-03-08 18:43:46 +0000

    Bump generated Firefox version to 3.6.15.

--- tools/uagen.pl
+++ tools/uagen.pl
@@ -56,9 +56,9 @@ use constant {
    # are too lazy to check, but want to change them anyway, take the values you
    # see in the "Help/About Mozilla Firefox" menu.
 
-   BROWSER_VERSION                   => "3.6.13",
-   BROWSER_REVISION                  => '1.9.2.13',
-   BROWSER_RELEASE_DATE              => '20101210',
+   BROWSER_VERSION                   => "3.6.15",
+   BROWSER_REVISION                  => '1.9.2.15',
+   BROWSER_RELEASE_DATE              => '20110305',
 };
 
 use constant LANGUAGES => qw(
