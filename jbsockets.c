e067f1e03b70389f030271604151951947b5c687
Fabian Keil <fabiankeil@users.sourceforge.net>
2011-05-03 09:55:35 +0000

    Prevent a NULL-pointer dereference in connect_to()
    It happened in case of address resolution failures when
    there was no forwarding proxy enabled for the request in
    the config file and forward-override{} wasn't used at all.
    
    Regression introduced in v1.98 by yours truly.
    
    Patch submitted by saperski in #3296527.

--- jbsockets.c
+++ jbsockets.c
@@ -158,6 +158,7 @@ jb_socket connect_to(const char *host, int portnum, struct client_state *csp)
       fd = no_rfc2553_connect_to(host, portnum, csp);
 #endif
       if ((fd != JB_INVALID_SOCKET) || (errno == EINVAL)
+         || (csp->fwd == NULL)
          || ((csp->fwd->forward_host == NULL) && (csp->fwd->type == SOCKS_NONE)))
       {
          break;
