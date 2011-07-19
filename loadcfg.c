69c116c94a2c77ee6d7ac58fef1e8cbe0c96b3f4
Fabian Keil <fabiankeil@users.sourceforge.net>
2011-07-19 11:00:20 +0000

    Minor style fix for load_config()

--- loadcfg.c
+++ loadcfg.c
@@ -1551,7 +1551,7 @@ struct configuration_spec * load_config(void)
       }
    }
 
-   for (i = 0; i < MAX_LISTENING_SOCKETS && NULL != config->haddr[i]; i++ )
+   for (i = 0; i < MAX_LISTENING_SOCKETS && NULL != config->haddr[i]; i++)
    {
       if ((*config->haddr[i] == '[')
          && (NULL != (p = strchr(config->haddr[i], ']')))
