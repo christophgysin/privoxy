1355718418a8c5fa8f6670bec0b447b009d03662
Fabian Keil <fabiankeil@users.sourceforge.net>
2012-05-24 14:57:49 +0000

    Accept a default-server-timeout of 0. It's the default after all.

--- loadcfg.c
+++ loadcfg.c
@@ -605,7 +605,7 @@ struct configuration_spec * load_config(void)
             if (*arg != '\0')
             {
                int timeout = atoi(arg);
-               if (0 < timeout)
+               if (0 <= timeout)
                {
                   config->default_server_timeout = (unsigned int)timeout;
                }
