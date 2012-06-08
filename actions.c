32634f464c9922c61b9014299eb79f9346c71dbd
Fabian Keil <fabiankeil@users.sourceforge.net>
2012-06-08 15:09:06 +0000

    Let load_one_actions_file() use ssplit() like every other function
    It doesn't matter whether or not ignore_leading is enabled.

--- actions.c
+++ actions.c
@@ -1400,7 +1400,7 @@ static int load_one_actions_file(struct client_state *csp, int fileid)
                return 1; /* never get here */
             }
 
-            num_fields = ssplit(version_string, ".", fields, SZ(fields), TRUE, FALSE);
+            num_fields = ssplit(version_string, ".", fields, SZ(fields), 1, 1);
 
             if (num_fields < 1 || atoi(fields[0]) == 0)
             {
