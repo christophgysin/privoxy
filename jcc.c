857361011124c4500cff8d808986873a38447666
Fabian Keil <fabiankeil@users.sourceforge.net>
2012-07-27 17:31:10 +0000

    In serve(), log the client socket and its condition before closing it
    This makes it more obvious that the socket actually gets closed
    and should help when diagnosing problems like #3464439.

--- jcc.c
+++ jcc.c
@@ -2630,6 +2630,10 @@ static void serve(struct client_state *csp)
 
    if (csp->cfd != JB_INVALID_SOCKET)
    {
+      log_error(LOG_LEVEL_CONNECT, "Closing client socket %d. "
+         "Keep-alive: %u, Socket alive: %u. Data available: %u.",
+         csp->cfd, 0 != (csp->flags & CSP_FLAG_CLIENT_CONNECTION_KEEP_ALIVE),
+         socket_is_still_alive(csp->cfd), data_is_available(csp->cfd, 0));
       close_socket(csp->cfd);
    }
 
