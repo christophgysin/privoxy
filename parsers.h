7079e9fbd584e502ae3ffee3b73033e3e81667b3
Fabian Keil <fabiankeil@users.sourceforge.net>
2012-09-04 08:33:17 +0000

    Add get_expected_content_length()

--- parsers.h
+++ parsers.h
@@ -63,6 +63,7 @@ extern jb_err sed(struct client_state *csp, int filter_server_headers);
 extern jb_err update_server_headers(struct client_state *csp);
 extern void get_http_time(int time_offset, char *buf, size_t buffer_size);
 extern jb_err get_destination_from_headers(const struct list *headers, struct http_request *http);
+extern unsigned long long get_expected_content_length(struct list *headers);
 
 #ifdef FEATURE_FORCE_LOAD
 extern int strclean(char *string, const char *substring);
