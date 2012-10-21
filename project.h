083e5053806cd46c14def826b153c25bada906be
Fabian Keil <fabiankeil@users.sourceforge.net>
2012-10-21 12:52:35 +0000

    Let the IOB_PEEK macro take the iob as argument

--- project.h
+++ project.h
@@ -412,10 +412,9 @@ struct iob
 
 /**
  * Return the number of bytes in the I/O buffer associated with the passed
- * client_state pointer.
- * May be zero.
+ * I/O buffer. May be zero.
  */
-#define IOB_PEEK(CSP) ((CSP->iob->cur > CSP->iob->eod) ? (CSP->iob->eod - CSP->iob->cur) : 0)
+#define IOB_PEEK(IOB) ((IOB->cur > IOB->eod) ? (IOB->eod - IOB->cur) : 0)
 
 
 /**
