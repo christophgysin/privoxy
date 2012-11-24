2989ce4c52ac98f8ddafbec5dbee74a8f186dabb
Fabian Keil <fabiankeil@users.sourceforge.net>
2012-11-24 13:55:51 +0000

    Increase MAX_AF_FILES to 30
    An unused file slot is pretty inexpensive.

--- project.h
+++ project.h
@@ -856,7 +856,7 @@ struct reusable_connection
  * Maximum number of actions/filter files.  This limit is arbitrary - it's just used
  * to size an array.
  */
-#define MAX_AF_FILES 10
+#define MAX_AF_FILES 30
 
 /**
  * Maximum number of sockets to listen to.  This limit is arbitrary - it's just used
