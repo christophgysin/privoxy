0497db2523b2f1c86d0efeb0ba027b6a6f0917bc
Fabian Keil <fabiankeil@users.sourceforge.net>
2012-12-27 15:48:53 +0000

    Remove a duplicated 'const' that should have been removed in 1.24

--- encode.c
+++ encode.c
@@ -47,7 +47,7 @@ const char encode_rcs[] = "$Id$";
 const char encode_h_rcs[] = ENCODE_H_VERSION;
 
 /* Maps special characters in a URL to their equivalent % codes. */
-static const char const url_code_map[256][4] = {
+static const char url_code_map[256][4] = {
    "",    "%01", "%02", "%03", "%04", "%05", "%06", "%07", "%08", "%09",
    "%0A", "%0B", "%0C", "%0D", "%0E", "%0F", "%10", "%11", "%12", "%13",
    "%14", "%15", "%16", "%17", "%18", "%19", "%1A", "%1B", "%1C", "%1D",
