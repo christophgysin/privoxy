ba401bc4b107851f9465f3a9c60ca5d90d27e0b3
Fabian Keil <fabiankeil@users.sourceforge.net>
2013-01-17 14:28:57 +0000

    Bump generated Firefox version to 17 (ESR)

--- tools/uagen.pl
+++ tools/uagen.pl
@@ -60,8 +60,8 @@ use constant {
    # are too lazy to check, but want to change them anyway, take the values you
    # see in the "Help/About Mozilla Firefox" menu.
 
-   BROWSER_VERSION                   => "16.0",
-   BROWSER_REVISION                  => '16.0',
+   BROWSER_VERSION                   => "17.0",
+   BROWSER_REVISION                  => '17.0',
    BROWSER_RELEASE_DATE              => '20100101',
 };
 
