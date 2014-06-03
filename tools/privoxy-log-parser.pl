8fa312abb641c02d5610c89c60ca6fc2cda46950
Fabian Keil <fabiankeil@users.sourceforge.net>
2014-06-03 10:26:21 +0000

    Accept multi-digit HTTP major and minor versions in highlight_request_line()

--- tools/privoxy-log-parser.pl
+++ tools/privoxy-log-parser.pl
@@ -646,7 +646,7 @@ sub highlight_request_line ($) {
 
         $rl = h('invalid-request') . $rl . h('Standard');
 
-    } elsif ($rl =~ m/^([-\w]+) (.*) (HTTP\/\d\.\d)/) {
+    } elsif ($rl =~ m/^([-\w]+) (.*) (HTTP\/\d+\.\d+)/) {
 
         # XXX: might not match in case of HTTP method fuzzing.
         # XXX: save these: ($method, $path, $http_version) = ($1, $2, $3);
