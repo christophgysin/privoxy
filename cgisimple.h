16bd78a85b44d4c0535e4ce6b62903265252fa09
Fabian Keil <fabiankeil@users.sourceforge.net>
2017-01-23 13:04:57 +0000

    Use a dedicated cgi handler to deal with tag-toggle requests
    As a result the /client-tags page is now safe to reach without
    trusted Referer header which makes bookmarking or linking to
    it more convenient.
    
    Finally, refreshing the /client-tags page to show the
    current state can no longer unintentionally repeat the
    previous toggle request.

--- cgisimple.h
+++ cgisimple.h
@@ -72,6 +72,9 @@ extern jb_err cgi_show_request (struct client_state *csp,
 extern jb_err cgi_show_client_tags(struct client_state *csp,
                                    struct http_response *rsp,
                                    const struct map *parameters);
+extern jb_err cgi_toggle_client_tag(struct client_state *csp,
+                                    struct http_response *rsp,
+                                    const struct map *parameters);
 #endif
 extern jb_err cgi_transparent_image (struct client_state *csp,
                                      struct http_response *rsp,
